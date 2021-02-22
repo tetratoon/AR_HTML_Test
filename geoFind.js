f8b59e7f2f27c38d998a7b468f9be6305e80fb3b
tetratoon <gerhardsprung@gmail.com>
2021-02-22 10:58:39 +0100

    encoded

--- geoFind.js
+++ geoFind.js
@@ -27,3 +27,47 @@ function geoFindMe() {
     }
 
 }
+function geoShowDirection(){
+    const status = document.querySelector('#status');
+    if('geolocation' in navigator) {
+        let heading = navigator.geolocation.heading;
+        if (heading){
+            status.textContent =`die Richtung =  ${heading}`;
+        } else  status.textContent =`die Richtung kann nicht bestimmt werden`;
+      } else {
+        status.textContent = `geolocation IS NOT available `;
+      }
+    
+}
+
+
+// AMCCQDVH
+// 62c5a320-0137-4e58-a28e-460e3f2e74c6
+
+function getData_a(){ 
+    const status = document.querySelector('#status');
+    axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            console.log(response.data);
+            status.textContent = response.data;
+        })
+        .catch(error => console.error(error));
+}
+
+
+function getData(){ 
+    const mongo = require('mongodb');
+    const MongoClient = mongo.MongoClient;
+    
+    //'mongodb://localhost:27017';
+    const url = 'mongodb://cluster0.3gk6d.mongodb.net/EAS2' 
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+
+        if (err) throw err;
+
+        console.log(client.topology.clientInfo);
+
+        client.close();
+    });
+}
+    
---
+++ server.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+const express = require('express');
+const server = express();
+
+const port = 4000;
+
+server.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
+
+server.get("/json", (req, res) => {
+    res.json({ message: "Hello world" });
+ });
+
+ server.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index1.html');
+ });
